33777abccbf2e897d861beaee81c1cb2dee23323
SirOswaldo <DeveloperOswaldo@gmail.com>
2021-05-30 12:35:42 -0400

    MorePrefixes.java Updated

--- MorePrefixes/src/main/java/org/kayteam/moreprefixes/MorePrefixes.java
+++ MorePrefixes/src/main/java/org/kayteam/moreprefixes/MorePrefixes.java
@@ -17,10 +17,21 @@
 
 package org.kayteam.moreprefixes;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.kayteam.moreprefixes.commands.MorePrefixesAdminCommand;
 import org.kayteam.moreprefixes.commands.MorePrefixesCommand;
+import org.kayteam.moreprefixes.listeners.AsyncPlayerPreLoginListener;
+import org.kayteam.moreprefixes.listeners.PlayerQuitListener;
+import org.kayteam.moreprefixes.placeholderapi.MorePrefixesExpansion;
+import org.kayteam.moreprefixes.playerdata.PlayerDataManager;
+import org.kayteam.moreprefixes.storage.Storage;
+import org.kayteam.moreprefixes.storage.storages.MySqlStorage;
+import org.kayteam.moreprefixes.storage.storages.YamlStorage;
+import org.kayteam.moreprefixes.util.papi.PlaceholderAPIUtil;
 import org.kayteam.moreprefixes.util.yaml.Yaml;
+
+import java.util.UUID;
 
 public class MorePrefixes extends JavaPlugin {
 
@@ -33,14 +44,50 @@ public class MorePrefixes extends JavaPlugin {
         return messages;
     }
 
-    @Override
-    public void onEnable() {
-        configuration.registerFileConfiguration();
-        messages.registerFileConfiguration();
-        getCommand("moreprefixesadmin").setExecutor(new MorePrefixesAdminCommand(this));
-        getCommand("moreprefixes").setExecutor(new MorePrefixesCommand(this));
-
+    private PlayerDataManager playerDataManager;
+    public PlayerDataManager getPlayerDataManager() {
+        return playerDataManager;
     }
 
+    private Storage storage;
+    public Storage getStorage() {
+        return storage;
+    }
 
+    @Override
+    public void onEnable() {
+        // Yaml
+        configuration.registerFileConfiguration();
+        messages.registerFileConfiguration();
+        // Storage
+        if ("MYSQL".equals(configuration.getFileConfiguration().getString("storage"))) {
+            storage = new MySqlStorage(this);
+        } else {
+            storage = new YamlStorage(this);
+        }
+        // PlayerData
+        playerDataManager = new PlayerDataManager(this);
+        // PlaceholderAPI
+        PlaceholderAPIUtil.registerExpansion(new MorePrefixesExpansion(this));
+        // Commands
+        getCommand("moreprefixesadmin").setExecutor(new MorePrefixesAdminCommand(this));
+        getCommand("moreprefixes").setExecutor(new MorePrefixesCommand(this));
+        // Listeners
+        getServer().getPluginManager().registerEvents(new AsyncPlayerPreLoginListener(this), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
+        // Load Online Players PlayerData
+        for (Player player:getServer().getOnlinePlayers()) {
+            UUID uuid = player.getUniqueId();
+            playerDataManager.load(uuid);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Unload Online Players PlayerData
+        for (Player player:getServer().getOnlinePlayers()) {
+            UUID uuid = player.getUniqueId();
+            playerDataManager.unload(uuid);
+        }
+    }
 }
