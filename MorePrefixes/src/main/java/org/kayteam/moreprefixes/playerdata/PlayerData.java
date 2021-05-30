d062a7f2929ff6c333180e7ae5158959f56af31a
SirOswaldo <DeveloperOswaldo@gmail.com>
2021-05-30 12:05:50 -0400

    PlayerData.java Finished.

---
+++ MorePrefixes/src/main/java/org/kayteam/moreprefixes/playerdata/PlayerData.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2021 SirOswaldo
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.kayteam.moreprefixes.playerdata;
+
+import java.util.List;
+
+public class PlayerData {
+
+    public PlayerData(List<String> prefixes) {
+        this.prefixes = prefixes;
+    }
+
+    private List<String> prefixes;
+    public List<String> getPrefixes() {
+        return prefixes;
+    }
+    public void setPrefixes(List<String> prefixes) {
+        this.prefixes = prefixes;
+    }
+
+}
